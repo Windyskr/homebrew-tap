9f2901ba47d8d27eb5e143eea4af88c1ab85396b
Windyskr <54269955+Windyskr@users.noreply.github.com>
2026-09-16 01:30:12 +0000

    acn: add 1.7.0 bottle.

--- Formula/acn.rb
+++ Formula/acn.rb
@@ -8,6 +8,13 @@ class Acn < Formula
   license "MIT"
   head "https://github.com/windyskr/agent-completion-notification.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Windyskr/homebrew-tap/releases/download/acn-1.7.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1d37e16a5d33ef4d77766009a849e0c64a0f3151705b1592230fdbfa8cea3644"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e519a715ecb5b659a2ecd448cf1384f5d932c74774c5b14e94798f24fe89e633"
+    sha256 cellar: :any,                 x86_64_linux:  "6ad2ba47550cd2cafd8eae16d9d45711f3cfa5b331a50df5376a540b37d0dcec"
+  end
+
   depends_on "go" => :build
 
   def install
